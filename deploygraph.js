b78b7a20337f7ed71e8301b89e836a2f97b3106a
genesisvlzqz <genesis.velazquez1@upr.edu>
2021-04-14 01:29:22 -0400

    Fixes to circles, scales, click, zoom, and background.
    Added picture background of map
    Added click function to each star
    Fixed inverted scale for x and y.
    Added color change on circle depending on star type.
    Added zoom functionality.

--- deploygraph.js
+++ deploygraph.js
@@ -1,21 +1,32 @@
-
-// set the dimensions and margins of the graph
+//April 13 log:
+//Added picture background of map
+//Added click function to each star
+//Fixed inverted scale for x and y.
+//Added color change on circle depending on star type
+//set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 50, left: 50},
-width = 900 - margin.left - margin.right,
-height = 540 - margin.top - margin.bottom;
+width = 780 - margin.left - margin.right,
+height = 485.4 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
-.append("svg")
-.attr("width", width + margin.left + margin.right)
-.attr("height", height + margin.top + margin.bottom)
-.append("g")
-.attr("transform",
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .call(d3.zoom().on("zoom", function () {
+  svg.attr("transform", d3.event.transform)
+  }))
+  .append("g")
+  .attr("transform",
   "translate(" + margin.left + "," + margin.top + ")");
 
+var mapimage = svg.append("image")
+  .attr("xlink:href","_images/starmap.png")
+  .attr("width", width)
+  .attr("height", height)
 
-//Read the data
-d3.json("https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=dec,ra,pl_hostname,pl_pnum&format=json", function(data) {
+  //Read the planet data
+  d3.json("https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=dec,ra,pl_hostname,pl_pnum,st_rad,st_teff&format=json", function(data) {
 
 
   //New array to insert data without dupes.
@@ -39,50 +50,42 @@ d3.json("https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI
   } 
 
 
-
-
   // Add X axis Bottom
   var x = d3.scaleLinear()
-  .domain([24, 0])
-  .range([ width,0]);
+  .domain([0,24])
+  .range([width,0])
   svg.append("g")
   .attr("transform", "translate(0," + height + ")")
   .call(d3.axisBottom(x));
 
-    // Add X axis Bottom
-    var xtop = d3.scaleLinear()
-    .domain([24, 0])
-    .range([ width, 0]);
-    svg.append("g")
-    .attr("transform", "translate(0," - height + ")")
-    .call(d3.axisBottom(xtop));
+    // Add X axis top
+    //var xtop = d3.scaleLinear()
+    //.domain([24, 0])
+    //.range([width, 0]);
+    //svg.append("g")
+    //.attr("transform", "translate(0," - height + ")")
+    //.call(d3.axisBottom(xtop));
 
 
   // Add Y axis
   var y = d3.scaleLinear()
   .domain([-90, 90])
-  .range([ height, 0]);
+  .range([height,0]);
   svg.append("g")
   .call(d3.axisLeft(y).ticks(6));
   
   // Add Right Y axis
-  var yright = d3.scaleLinear()
-  .domain([-90, 90])
-  .range([ height, 0]);
-  svg.append("g")
-  .attr("transform", "translate("+width+",0)")
-  .call(d3.axisRight(yright).ticks(6));
+ // var yright = d3.scaleLinear()
+ // .domain([-90, 90])
+  //.range([0,height]);
+  //svg.append("g")
+  //.attr("transform", "translate("+width+",0)")
+  //.call(d3.axisRight(yright).ticks(6));
 
   // Add a scale for bubble size
   var z = d3.scaleLinear()
   .domain([200000, 1310000000])
   .range([ 4, 40]);
-
-
-  // var myimage = svg.append('image')
-  //   .attr('xlink:href', 'https://www.evernote.com/shard/s25/sh/0a6c1268-9fe0-4687-ac30-164ed59c30ee/33fab35197b3b7e98c217ef7f5b05ae7/res/81a02713-357f-cb01-3953-9d2d0474ac12')
-  //   .attr('width', width + margin.left + margin.right)
-  //   .attr('height', height + margin.top + margin.bottom)
 
   // -1- Create a tooltip div that is hidden by default:
   var tooltip = d3.select("#my_dataviz")
@@ -105,6 +108,9 @@ d3.json("https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI
     .style("left", (d3.mouse(this)[0]+30) + "px")
     .style("top", (d3.mouse(this)[1]+30) + "px")
   }
+  var viewsystem = function(d) {
+    window.open("https://exoplanetarchive.ipac.caltech.edu/overview/" + d.pl_hostname, "_blank")
+  }
   var moveTooltip = function(d) {
     tooltip
     .style("left", (d3.mouse(this)[0]+30) + "px")
@@ -116,6 +122,41 @@ d3.json("https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI
     .duration(200)
     .style("opacity", 0)
   }
+
+  //Set circle color based on temperature
+  var setColor = function(d) {
+
+    if (d.st_teff<3500)
+    {
+      return "red";
+    }
+    else if (d.st_teff>3500 && d.st_teff<5000)
+    {
+      return "orange";
+    }
+    else if (d.st_teff>5000 && d.st_teff<6000)
+    {
+      return "yellow";
+    }
+    else if (d.st_teff>6000 && d.st_teff<7500)
+    {
+      return "white";
+    }
+    else if (d.st_teff>7500 && d.st_teff<11000)
+    {
+      return "black";
+    }    
+    else if (d.st_teff>11000 && d.st_teff<25000)
+    {
+      return "blue";
+    }
+    else 
+    {
+      return "black";
+    }
+
+  }
+
 
   svg.append("text")
   .attr("class", "x label")
@@ -141,12 +182,13 @@ d3.json("https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI
   .enter()
   .append("circle")
   .attr("class", "bubbles")
-  .attr("cx", function (d) { return x(d.ra) * (67/1000); } )
+  .attr("cx", function (d) { return x(d.ra* (1/15)); } ) //1 hour RA = 15 Degrees RA
   .attr("cy", function (d) { return y(d.dec); } )
-  .attr("r", 5 )
-  .style("fill", "black")
-  .style("opacity", 0.4)
+  .attr("r", 3)
+  .style("fill", setColor)
+  .style("opacity", 1)
     // -3- Trigger the functions
+    .on("click", viewsystem)
     .on("mouseover", showTooltip )
     .on("mousemove", moveTooltip )
     .on("mouseleave", hideTooltip )
